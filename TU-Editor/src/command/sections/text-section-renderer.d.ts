8240d45acce7989c2b6fe810f0dfd05326fa5917
CIVITAS-John <CIVITAS-John@users.noreply.github.com>
2023-05-17 15:21:39 +0000

    deploy: 9492d3ff35fb7855ad4b77fc739cdd8e4f3f911d

--- TU-Editor/src/command/sections/text-section-renderer.d.ts
+++ TU-Editor/src/command/sections/text-section-renderer.d.ts
@@ -1,8 +1,12 @@
+import { ChatResponseSection } from "../../chat/client/chat-response";
+import { UIRendererOf } from "../outputs/ui-renderer";
 import { SectionRenderer } from "./section-renderer";
 /** TextSectionRenderer: A block that displays the a text response section. */
 export declare class TextSectionRenderer extends SectionRenderer {
     /** Constructor: Create a new UI renderer. */
     constructor();
+    /** SetData: Set the data to render. */
+    SetData(Data: ChatResponseSection): UIRendererOf<ChatResponseSection>;
     /** RenderInternal: Render the UI element. */
     protected RenderInternal(): void;
 }

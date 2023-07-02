a23b9d4ed2929ce0dc30770d3127aeb9ab45aba0
CIVITAS-John <CIVITAS-John@users.noreply.github.com>
2023-07-02 18:15:59 +0000

    deploy: ca5d4e491fc2c5a68d60996aea947fb90a0bc62d

--- tortuga-editor.js
+++ tortuga-editor.js
@@ -26446,7 +26446,7 @@
         'Compile error unknown': (Number) => `Sorry, there is an unknown error. Please report it as a bug.`,
         'Compile error in model': () => `Sorry, there are errors in the open project. Please fix the issue in the code tab first.`,
         'Showing full text help of _': (Name) => `Here is the help information of [${Name}](<observer=help ${Name} -full>).`,
-        'Arguments needed for execution _': (Name, Arguments) => `The "${Name}" procedure needs argument${Arguments > 1 ? 's' : ''} to run. Please provide them.`,
+        'Arguments needed for execution _': (Name, Arguments) => `The "${Name}" procedure needs ${Arguments > 1 ? '' : 'an '}argument${Arguments > 1 ? 's' : ''} to run. Please provide ${Arguments > 1 ? 'them' : 'it'}.`,
         'Please download Turtle Universe': () => `The feature is unavailable in Web Preview. Please download [Turtle Universe](https://www.turtlesim.com/products/turtle-universe/) to continue.`,
         'Failed to retrieve knowledge': () => `Sorry, we did not find the specific knowledge.`,
         // Options
@@ -27179,8 +27179,29 @@
     }
     catch (error) { }
 
+    /** Global: Global object. */
+    const Global = typeof globalThis === 'undefined' ? window : globalThis;
     /** Log: Log to console if debug is enabled. */
-    const Log = !globalThis.GalapagosSilent ? console.log : () => { };
+    const Log = Global.GalapagosSilent ? console.log : () => { };
+    /**
+     * String.prototype.trimStart() polyfill
+     * Adapted from polyfill.io
+     */
+    if (!String.prototype.trimStart)
+        String.prototype.trimStart = function () {
+            return this.replace(new RegExp('^' +
+                /[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/
+                    .source, 'g'), '');
+        };
+    /**
+     * String.prototype.trimEnd() polyfill
+     * Adapted from polyfill.io
+     */
+    if (!String.prototype.trimEnd)
+        String.prototype.trimEnd = function () {
+            return this.replace(new RegExp(/[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/
+                .source + '$', 'g'), '');
+        };
 
     /** GetCursorUntilMode: Get the cursor until the "mode" node. */
     function GetCursorUntilMode(State) {
